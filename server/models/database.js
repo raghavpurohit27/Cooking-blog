07476cf5e229562b9e254243d7ee5b7ee6e9a172
raghav <raghavmd2731@gmail.com>
2022-09-17 19:57:41 +0530

    third commit

--- server/models/database.js
+++ server/models/database.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONOGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.MONOGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
-  console.log('Connected')
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function(){
+//   console.log('Connected')
+// });
+
+
+
+const DB = process.env.MONOGODB_URI;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+    
+  })
+  .then(() => {
+    console.log('Database Connected');
+  });
+
 
 //models
 require('./Category');
 require('./Recipe');
+
+
+
